855ac1af3aa981ba7d13e35e901ebee9819586d0
Kaio <kaioaac3@gmail.com>
2026-06-28 22:28:09 -0300

    testando instalar app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import { useEffect, useState } from "react";
 import { AppRoutes } from "./routes/router";
-import { PWAInstallBanner } from "./assets/styles/componentes/PWAInstallBanner";
-
-const BANNER_DELAY_MS = 3000;
 
 export const App = () => {
-  const [showBanner, setShowBanner] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setShowBanner(true), BANNER_DELAY_MS);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <>
-      <AppRoutes />
-      {showBanner && <PWAInstallBanner />}
-    </>
-  );
+  return <AppRoutes />;
 };
