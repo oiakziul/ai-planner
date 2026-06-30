49888fca8adee01bfa58d378c8505e3e154d35c0
Kaio <kaioaac3@gmail.com>
2026-06-30 12:16:52 -0300

    Refatorando arquivos antigos

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,12 +1,13 @@
-
+//Componentes
 export * from './Header';
 export * from './Home';
 export * from '../context/ThemeContext';
 export * from './Footer'
 
-export * from './Inicio';
+
 
 //Paginas
+export * from './Inicio';
 
 export * from './Pagina1';
 export * from './Pagina2';
--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,6 @@ export default defineConfig({
             purpose: "maskable",
           },
         ],
-        // ↓ cola aqui
         screenshots: [
           {
             src: "screenshot-mobile.png",
